fac6b3873240ff2e2df3bd890116c6a04e220fb0
Roseli7 <92137741+Roseli7@users.noreply.github.com>
2021-10-15 10:23:48 -0300

    Update my.c

--- my/my.c
+++ my/my.c
@@ -2,9 +2,6 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-module_init(my_init);
-module_exit(my_exit);
-
 static int __init my_init(void)
 {
     printk(KERN_INFO "+++ module succefully loaded!\n");
@@ -15,6 +12,9 @@ static void __exit my_exit(void)
 {
     printk(KERN_INFO "+++ module succefully unloaded!\n");
 }
+
+module_init(my_init);
+module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("X-Shar");
