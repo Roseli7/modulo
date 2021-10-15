97d29188db4fbc462664c6ca512fb2a43213d661
Roseli7 <92137741+Roseli7@users.noreply.github.com>
2021-10-15 07:11:06 -0300

    Add files via upload

---
+++ rootkit/main.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
+
+static int __init rootkit_init(void)
+{
+    printk(KERN_INFO "+++ Rootkit module succefully loaded !\n");
+}
+static void __exit rootkit_exit(void)
+{
+    printk(KERN_INFO "+++ Rootkit module succefully unloaded !\n");
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("neo");
+MODULE_DESCRIPTION("LKM rootkit");
+MODULE_VERSION("ALL");
+
